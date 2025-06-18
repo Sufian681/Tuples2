7c6d0ec84ac3fa6e3d196d686cca6c5808859593
Sufi <sufiansonic@gmail.com>
2025-06-18 19:53:54 +0900

    Nahiansigmaboiii

---
+++ Tuple Operations.py
@@ -0,0 +1,18 @@
+tuplex = ("yay", False, 90, 8.99)
+
+print(tuplex)
+print(type(tuplex))
+print(tuplex[3])
+
+t1 = (90, 80, 70, 30, 20, 80)
+print(t1)
+
+t1 = t1 + ("Sufian", "Nahian")
+print(t1)
+
+tuple1 = (50,100,150,50,50,50,50)
+
+print(tuple1.count(50))
+print()
+
+print(t1[::-1])
